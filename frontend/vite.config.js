bf42696f39af8190f95a3b1c77c60d09cbf0634b
Hakimchamakhi <78047649+Hakimchamakhi@users.noreply.github.com>
2025-12-16 00:38:02 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: true, // <--- This fixes the "Blocked request" error
+  },
   server: {
     port: 3000,
     proxy: {
